9fb0e89c8ba46adcc90aca238825d9d786b97ec6
Eunong <hoodyong@gmail.com>
2022-10-08 23:55:44 +0900

    programmers hash practice 42578 수정 완료

--- src/algorithms/programmers/Test42578.java
+++ src/algorithms/programmers/Test42578.java
@@ -57,9 +57,45 @@ public class Test42578 {
         }
     }
 
+    // 계산식으로 풀기
+    /*
+    만약 상의 2개, 하의 3개가 있다면
+    상의a, 상의b, 상의x
+    하의a, 하의b, 하의c, 하의x
+    <선택할 수 있는 가지수>
+       상의a+하의a, 상의a+하의b, 상의a+하의c, 상의a+하의x
+       상의b+하의a, 상의b+하의b, 상의b+하의c, 상의b+하의x
+       상의x+하의a, 상의x+하의b, 상의x+하의c, 상의x+하의x
+    (상의2개와 상의를 안입는 경우) * (하의3개와 하의를 안입는 경우) 두 수의 곱으로 풀 수 있다.
+    이 때, 상의와 하의를 모두 입지 않은 (상의x+하의x)의 경우. 즉 전체를 x를 고를 수 있는 경우 1은 항상 빼준다.
+
+     */
+    public static int solution2(String[][] clothes) {
+        int sum = 1;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        // 의상 종류 별로 몇 건이 있는지 카운팅
+        for(int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        // 종류 별 의상 가지수+1(안입는경우)를 곱해준다.
+        for(int cnt : map.values()) {
+            sum *= (cnt+1);
+        }
+
+        return sum-1; // 전체를 안입는 경우 1을 빼준다.
+    }
+
     public static void main(String[] args) {
-        System.out.println("TestCase1 : " + solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
-        System.out.println("TestCase2 : " + solution(new String[][]{{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
+        // 시간초과
+//        System.out.println("TestCase1 : " + solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+//        System.out.println("TestCase2 : " + solution(new String[][]{{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
+
+        System.out.println("TestCase1 : " + solution2(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+        System.out.println("TestCase2 : " + solution2(new String[][]{{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
+        System.out.println("TestCase3 : " + solution2(new String[][]{{"동그란 안경", "얼굴"}, {"검정 선글라스", "얼굴"}, {"파란색 티셔츠", "상의"}, {"청바지", "하의"}, {"긴 코트", "겉옷"}}));
     }
 
 }
