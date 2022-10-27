e360dffabfcca753b8cf64ce89138156b04a27c8
Eunong <hoodyong@gmail.com>
2022-10-28 01:56:30 +0900

    프로그래머스 완전탐색 dfs 문제 87946 피로도 - 완료

--- src/algorithms/programmers/exhaustiveSearch/Test87946.java
+++ src/algorithms/programmers/exhaustiveSearch/Test87946.java
@@ -10,38 +10,66 @@ public class Test87946 {
 
     public static int solution(int k, int[][] dungeons) {
         visitCnt = 0;
-        max = -1;
-        int answer = -1;
-
+        max = 0;
         boolean[] check = new boolean[dungeons.length];
         remaining = k;
 
-        permutation(k, dungeons, check, 0, dungeons.length, dungeons.length);
+        dfs(k, dungeons, check, 0);
+        // permutation(k, dungeons, check, 0, dungeons.length, dungeons.length);
 
-        return answer;
+        return max;
+    }
+
+    public static void dfs(int k, int[][] dungeons, boolean[] check, int cnt) { // 3개 순열로 선택
+        if(max == dungeons.length) { // 최대만큼 탐험했을 경우 더 이상 탐색하지 않음
+            return;
+        }
+
+        for(int i = 0; i < dungeons.length; i++) {
+            if(!check[i] && dungeons[i][0] <= k) {
+                check[i] = true;
+                dfs(k-dungeons[i][1], dungeons, check, cnt+1);
+                check[i] = false;
+            }
+        }
+
+        max = Math.max(cnt, max);
     }
 
     public static void permutation(int k, int[][] dungeons, boolean[] check, int depth, int n, int r) {
-        if(depth == r) {
-            System.out.println("depth : " + depth + ", r : " + r);
-            remaining = k;
-            max = (visitCnt > max) ? visitCnt : max;
+        if(max == n) { // 최대만큼 탐험했을 경우 종료
+            return;
+        }
+        if(depth == r) { // 모두 탐험했을 경우 종료
+            max = Math.max(visitCnt, max);
+            return;
         }
 
         for(int i = 0; i < n; i++) {
             if(!check[i]) {
+                int minus = 0;
                 check[i] = true;
+
                 if(dungeons[i][0] <= remaining) {
-                    remaining -= dungeons[i][1];
                     visitCnt++;
+                    remaining -= dungeons[i][1];
+                    minus = dungeons[i][1];
                 }
                 permutation(k, dungeons, check, depth+1, n, r);
                 check[i] = false;
+                remaining += minus;
+                if(dungeons[i][0] <= remaining) {
+                    visitCnt--;
+                }
+
             }
         }
+
     }
 
     public static void main(String[] args) {
-        System.out.println("TestCase1 : " + solution(80, new int[][]{{80, 20}, {50, 40}, {30, 10}}));
+        System.out.println("TestCase1 : " + solution(80, new int[][]{{80, 20}, {50, 40}, {30, 10}})); // 3
+        System.out.println("TestCase2 : " + solution(80, new int[][]{{100, 20}, {50, 40}, {80, 10}})); // 2
+        System.out.println("TestCase3 : " + solution(80, new int[][]{{100, 20}, {100, 40}, {100, 10}})); // 0
     }
 }
