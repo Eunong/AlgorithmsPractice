d74d377abc44c874540821e4d75cddfd6c173c96
Eunong <hoodyong@gmail.com>
2022-11-14 01:43:28 +0900

    프로그래머스 greedy 문제 42860 조이스틱 - 로직 설명 추가

--- src/algorithms/programmers/greedy/Test42860.java
+++ src/algorithms/programmers/greedy/Test42860.java
@@ -11,7 +11,7 @@ public class Test42860 {
         int cnt = 0;
 
         int len = name.length();
-        int minMove = len - 1; // case1) 계속 오른쪽으로만 이동(최대 좌우이동횟수) => 문자열의 길이 - 1
+        int minMove = len - 1; // 계속 오른쪽으로만 이동(최대 좌우이동횟수) => 문자열의 길이 - 1
 
         char alphabet;
 
@@ -27,11 +27,10 @@ public class Test42860 {
                 cnt += 'Z' - alphabet + 1;
             }
 
-            // 좌우이동 계산 - case1, case2 중 최소값 선택
-            // case1) 계속 오른쪽으로만 이동 => 문자열의 길이 - 1
-            // case2) 오른쪽으로 이동 중 A를 만나면 왼쪽으로 이동
-
-            // 좌우 이동 계산
+            // 좌우이동 계산 - 좌우 이동 횟수를 count 한다. A는 변경할 필요가 없으므로 A가 모여있는 곳으로는 이동하지 않아도 됨.
+            // 아래 두 케이스 중 최소 경우를 선택한다.
+            // (1) 좌측부터 A를 만날 때까지 전진한 후 다시 돌아와서 우측 끝으로 돌아가 A가 아닌 수까지 이동하는 경우
+            // (2) 최초부터 우측 끝으로 돌아가 A가 아닌 수까지 이동한 후 다시 원점으로 돌아와 시작점부터 A가 아닌 문자까지 이동하는 경우
             int idx = i + 1;
 
             // 다음 글자가 A인 경우 idx 증가
@@ -40,7 +39,10 @@ public class Test42860 {
             }
             
             // 좌우 이동 비교
-            minMove = Math.min(minMove, i +(len-idx) + Math.min(i,len-idx));
+            // i : 처음 위치부터 현재 문자열까지 오기 위한 횟수
+            // len-idx : 바로 옆에 연결된 A를 제외한 현재 문자의 오른쪽 문자열의 길이
+            // Math.min(i, len-idx) : 0 -> i까지 갔다가 다시 시작점으로 되돌아 오는 경우 또는 맨 처음부터 뒤로 갔다가 원점으로 되돌아오는 경우 중 빠른 경우 선택
+            minMove = Math.min(minMove, i + (len-idx) + Math.min(i, len-idx));
         }
         
         return cnt + minMove;
@@ -50,5 +52,8 @@ public class Test42860 {
         System.out.println("TestCase1 : " + solution("JEROEN")); // 56
         System.out.println("TestCase2 : " + solution("JAN")); // 23
         System.out.println("TestCase3 : " + solution("BBAABB")); // 8
+        System.out.println("TestCase4 : " + solution("BBBBAAAABA")); // 12
+        System.out.println("TestCase5 : " + solution("AAABBBBBBBAA")); // 16
+        System.out.println("TestCase6 : " + solution("BBAABAB")); // 9
     }
 }
